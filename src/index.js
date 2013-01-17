8a8cbcbab61799824b7d13a9569ab1c4ebc07677
Peter van der Zee <zee@uxebu.com>
2013-01-17 01:28:51 +0100

    Initial release

---
+++ src/index.js
@@ -0,0 +1,64 @@
+/**
+ * Parse input with zeparser2, rewrite html literals to proper (DOM api) js
+ */
+function translate(input){
+  // take apart
+  var par = new Par(input);
+  par.run();
+  var tokens = par.tok.tokens;
+
+  // reconstruct
+  return tokens.map(function(token){
+    if (token.type !== HTML) return token.value;
+    console.log(token.root)
+    return reviveTag(token.root);
+  }).join('');
+}
+
+function reviveTag(tag, _depth){
+  var str = 'document.createElement(\''+tag.tagName.replace(/'/g,'\\\'')+'\')';
+  var prefix = Array.call(null,(_depth|0)+1).join('\t');
+  var prefix2 = prefix + '\t';
+
+  if (tag.attributes || tag.children) {
+    str =
+      '(function(){\n' +
+        prefix2+'var dom = '+str+';\n';
+
+    if (tag.attributes) {
+      tag.attributes.forEach(function(attr){
+        if (attr.nameType === 'dynamic') {
+          str +=
+            prefix2+'var name = function(){ return '+attr.value+'; }();\n' +
+            prefix2+'if (name !== \'\') dom.setAttribute(\'\\\'\'+name.replace(/\'/g,\'\\\'\')+\'\\\'\', true);\n';
+        } else {
+          str += prefix2+'dom.setAttribute(\''+attr.name.replace(/'/g,'\\\'')+'\', ';
+
+          if (attr.valueType === 'none') str += 'true';
+          else if (attr.valueType === 'normal') str += '\''+String(attr.value).replace(/'/g,'\\\'')+'\'';
+          else str += 'function(){ return '+attr.value+'; }()';
+
+          str += ');\n';
+        }
+      });
+    }
+
+    if (tag.children) {
+      tag.children.forEach(function(child){
+        if (child.type === 'text') str += prefix2+'dom.appendChild(document.createTextNode(\''+child.value.replace(/'/g,'\\\'')+'\'));\n';
+        else if (child.type === 'tag') str += prefix2+'dom.appendChild('+reviveTag(child.tag, (_depth|0)+1)+');\n';
+        else if (child.type === 'dynamic') {
+          str +=
+            prefix2+'var value = function(){ return '+child.value+'; }();\n' +
+            prefix2+'if (typeof value === \'object\' && value !== null) dom.appendChild(result);\n' +
+            prefix2+'else if (typeof value !== \'\') dom.appendChild(document.createTextNode(String(result)));\n' +
+          '';
+        }
+      });
+    }
+
+    str += prefix+'})()';
+  }
+
+  return str;
+}
